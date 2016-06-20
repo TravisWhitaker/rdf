15a19bf02187f845e5155e6ed12e86b94ec360a0
Travis Whitaker <pi.boy.travis@gmail.com>
2016-06-20 14:35:00 -0400

    Initial commit, add RDF types.

---
+++ src/Data/RDF/Types.hs
@@ -0,0 +1,91 @@
+{-|
+Module      : Data.RDF.Types
+Description : Representation and Incremental Processing of RDF Data
+Copyright   : Travis Whitaker 2016
+License     : MIT
+Maintainer  : pi.boy.travis@gmail.com
+Stability   : Provisional
+Portability : Portable
+
+RDF types.
+-}
+
+{-# LANGUAGE BangPatterns
+           , DeriveAnyClass
+           #-}
+
+module Data.RDF.Types where
+
+import qualified Data.Text as T
+
+type RDFGraph = [Triple]
+
+data Triple = Triple !Subject !Predicate !Object
+              deriving ( Eq
+                       , Ord
+                       , Read
+                       , Show
+                       )
+
+data Subject = IRISubject   !IRI
+             | BlankSubject !BlankNode
+             deriving ( Eq
+                      , Ord
+                      , Read
+                      , Show
+                      )
+
+newtype Predicate = Predicate { unPredicate :: IRI }
+                  deriving ( Eq
+                           , Ord
+                           , Read
+                           , Show
+                           )
+
+data Object = IRIObject     !IRI
+            | LiteralObject !Literal
+            | BlankObject   !BlankNode
+            deriving ( Eq
+                     , Ord
+                     , Read
+                     , Show
+                     )
+
+-- | A blank node with it's local label, without the preceeding "_:".
+newtype BlankNode = BlankNode { unBlankNode :: T.Text }
+                  deriving ( Eq
+                           , Ord
+                           , Read
+                           , Show
+                           )
+
+data Literal = Literal {
+    litString :: !T.Text
+  , litType   :: !(Maybe IRI)
+  } deriving ( Eq
+             , Ord
+             , Read
+             , Show
+             )
+
+data IRI = IRI {
+    iriScheme   :: !T.Text
+  , iriAuth     :: !(Maybe IRIAuth)
+  , iriPath     :: !T.Text
+  , iriQuery    :: !(Maybe T.Text)
+  , iriFragment :: !(Maybe T.Text)
+  } deriving ( Eq
+             , Ord
+             , Read
+             , Show
+             )
+
+data IRIAuth = IRIAuth {
+    iriUser :: !(Maybe T.Text)
+  , iriHost :: T.Text
+  , iriPort :: !(Maybe T.Text)
+  } deriving ( Eq
+             , Ord
+             , Read
+             , Show
+             )
