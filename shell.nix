32a023ae59ea091b5039b3353231512c0d54e3e1
Travis Whitaker <pi.boy.travis@gmail.com>
2022-09-27 13:15:36 -0700

    bumps

--- shell.nix
+++ shell.nix
@@ -2,13 +2,13 @@ let pinned-nixpkgs = builtins.fetchGit
     {
         url = "https://github.com/nixos/nixpkgs";
         ref = "master";
-        rev = "80cef4476a6d3c2a7cb5ad46c4490eed7ac39a28";
+        rev = "859516a1a5af797ab11ac7597aa77a670877d87f";
     };
 in with import pinned-nixpkgs {};
 runCommand "rdf-env"
 {
     buildInputs =
-        let thisghc = haskell.packages.ghc8104.ghcWithPackages
+        let thisghc = haskell.packages.ghc924.ghcWithPackages
             (p: [ p.cabal-install
                   p.ghcid
                 ]);
